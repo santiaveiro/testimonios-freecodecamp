a655fbfe55c62202863957e295e4dbbaca216444
Santiago Aveiro <aveiro.santiago.g@gmail.com>
2023-07-01 09:58:44 -0300

    Repositorio listo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './components/testimonio.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio 
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nulla neque tempora unde voluptas obcaecati, quia nesciunt eos natus laboriosam provident excepturi ipsam est quasi asperiores quidem, delectus eaque recusandae.' />
+        <Testimonio 
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='shawn'
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nulla neque tempora unde voluptas obcaecati, quia nesciunt eos natus laboriosam provident excepturi ipsam est quasi asperiores quidem, delectus eaque recusandae.' />
+        <Testimonio 
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nulla neque tempora unde voluptas obcaecati, quia nesciunt eos natus laboriosam provident excepturi ipsam est quasi asperiores quidem, delectus eaque recusandae.' />
+      </div>
     </div>
   );
 }
